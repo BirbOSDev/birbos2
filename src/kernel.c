1802b51a611ee752da62e313853a205531ab751d
NSG650 <ankitanubha5@gmail.com>
2020-05-03 16:02:50 +0000

    Update src/shell.c, src/kernel.c files

--- src/kernel.c
+++ src/kernel.c
@@ -3,19 +3,18 @@
 #include "../include/idt.h"
 #include "../include/util.h"
 #include "../include/shell.h"
-kmain()
-{
+kmain(){
 	isr_install();
 	clearScreen();
-	print("Booting Up Birb\nSucessfully Booted up\n");
+	print("Booting Up Birb OS\nSucessfully Booted up\n");
 	print("\n\n");
-	print("oooooo____oo__________oo____________oooo_____ooooo__\n");
-	print("oo____oo______oo_ooo__oooooo______oo____oo__oo___oo_\n");
-	print("oooooooo__oo__ooo___o_oo___oo____oo______oo__oo_____\n");
-	print("oo____oo__oo__oo______oo___oo____oo______oo____oo___\n");
-	print("oo____oo__oo__oo______oo___oo_____oo____oo__oo___oo_\n");
-	print("ooooooo__oooo_oo______oooooo________oooo_____ooooo__\n");
+	print("oooooo____oo__________oo_________oooo_____ooooo__\n");
+	print("oo____oo______oo_ooo__oooooo___oo____oo__oo___oo_\n");
+	print("oooooooo__oo__ooo___o_oo___oo_oo______oo__oo_____\n");
+	print("oo____oo__oo__oo______oo___oo_oo______oo____oo___\n");
+	print("oo____oo__oo__oo______oo___oo__oo____oo__oo___oo_\n");
+	print("ooooooo__oooo_oo______oooooo_____oooo_____ooooo__\n");
 	print("____________________________________________________\n");
-	print("\n\nWelcome to Birb\n");
-    launch_shell(0);    
+	print("\n\nWelcome to Birb OS\n");
+    	launch_shell(0);
 }
--- src/shell.c
+++ src/shell.c
@@ -3,44 +3,39 @@ void launch_shell(int n)
 {
 	string ch = (string) malloc(200); // util.h
 	int counter = 0;
-	do
-	{
+	do{
                     print(">>>");
 		    ch = readStr(); //memory_copy(readStr(), ch,100);
-		    if(strEql(ch,"clear"))
-		    {
+		    if(strEql(ch,"clear")){
 		            clearScreen();
 		    }
-		    else if(strEql(ch,"echo"))
-		    {
+		    else if(strEql(ch,"echo")){
 		    	echo();
 		    }
-		    else if(strEql(ch,"ls"))
-		    {
+		    else if(strEql(ch,"ls")){
 			print("\nbruh there is no file system\n");
 		    }
-		    else if(strEql(ch,"about"))
-		    {
+		    else if(strEql(ch,"about")){
 			  print("\n");
-			  print("oooooo____oo__________oo____________oooo_____ooooo__\n");
-		          print("oo____oo______oo_ooo__oooooo______oo____oo__oo___oo_\n");
-		          print("oooooooo__oo__ooo___o_oo___oo____oo______oo__oo_____\n");
-		          print("oo____oo__oo__oo______oo___oo____oo______oo____oo___\n");
-        		  print("oo____oo__oo__oo______oo___oo_____oo____oo__oo___oo_\n");
-        		  print("ooooooo__oooo_oo______oooooo________oooo_____ooooo__\n");
+			  print("oooooo____oo__________oo__________oooo_____ooooo__\n");
+		          print("oo____oo______oo_ooo__oooooo____oo____oo__oo___oo_\n");
+		          print("oooooooo__oo__ooo___o_oo___oo__oo______oo__oo_____\n");
+		          print("oo____oo__oo__oo______oo___oo__oo______oo____oo___\n");
+        		  print("oo____oo__oo__oo______oo___oo___oo____oo__oo___oo_\n");
+        		  print("ooooooo__oooo_oo______oooooo______oooo_____ooooo__\n");
         		  print("____________________________________________________\n");
 		          print("\nBirb OS 1.0.0\n");
 		    }
-		    else if(strEql(ch,"help"))
-		    {
+		    else if(strEql(ch,"help")){
 		    	help();
 		    }
-		    else if(strEql(ch,""))
-		    {
+		    else if(strEql(ch,"calc")){
+			calc();
+		    }
+		    else if(strEql(ch,"")){
 			print("\n");
 		    }
-		    else
-		    {
+		    else{
 		            print("\nBad command!\n");
 		            print("");
 		    } 
@@ -50,19 +45,16 @@ void launch_shell(int n)
 
 
 
-void echo()
-{
+void echo(){
 	print("\n");
 	string str = readStr();
 	print("\n");
 	print(str);
 	print("\n");
 }
-void print_array(int arr[],int n)
-{
+void print_array(int arr[],int n){
 	int i = 0;
-	for (i = 0;i<n;i++)
-	{
+	for (i = 0;i<n;i++){
 		/*print("ARR[");
 		print(int_to_string(i));
 		print("]: ");*/
@@ -72,12 +64,10 @@ void print_array(int arr[],int n)
 	print("\n");
 }
 
-int sum_array(int arr[],int n)
-{
+int sum_array(int arr[],int n){
 	int i = 0;
 	int s = 0;
-	for (i = 0;i<n;i++)
-	{
+	for (i = 0;i<n;i++){
 		s += arr[i];
 	}
 	return s;
@@ -85,14 +75,11 @@ int sum_array(int arr[],int n)
 
 
 
-void print_matrix(int matrix[][100],int rows,int cols)
-{
+void print_matrix(int matrix[][100],int rows,int cols){
 	int i =0;
 	int j = 0;
-	for (i = 0;i<rows;i++)
-	{
-		for(j =0;j<cols;j++)
-		{
+	for (i = 0;i<rows;i++){
+		for(j =0;j<cols;j++){
 			print(int_to_string(matrix[i][j]));
 			print("   ");
 		}
@@ -103,10 +90,68 @@ void print_matrix(int matrix[][100],int rows,int cols)
 
 
 
-void help()
-{
+void help(){
 	print("\nclear     : Clears the screen");
 	print("\necho      : Reprint a given text");
-	print("\nabout     : Prints out the version number of os");
+	print("\nabout     : Prints outs detail of the os");
+	print("\ncalc      : Calculator");
 	print("\n\n");
+}
+
+void add(){
+	print("\nNumber 1: ");
+	int x = str_to_int(readStr());
+	print("\nNumber 2: ");
+	int y = str_to_int(readStr());
+	print("\nResult: ");
+	print(int_to_string(x+y));
+	print("\n");
+}
+void multiply(){
+        print("\nNumber 1: ");
+        int x = str_to_int(readStr());
+        print("\nNumber 2: ");
+        int y = str_to_int(readStr());
+        print("\nResult: ");
+        print(int_to_string(x*y));
+        print("\n");
+}
+void sub(){
+        print("\nNumber 1: ");
+        int x = str_to_int(readStr());
+        print("\nNumber 2: ");
+        int y = str_to_int(readStr());
+        print("\nResult: ");
+        print(int_to_string(x-y));
+        print("\n");
+}
+void div(){
+        print("\nNumber 1: ");
+        int x = str_to_int(readStr());
+        print("\nNumber 2: ");
+        int y = str_to_int(readStr());
+        print("\nResult: ");
+	int z = x/y;
+        print(int_to_string(z));
+        print("\n");
+}
+
+void calc(){
+	print("\nEnter 1 to add, 2 to subtract, 3 to multiply and 4 to divide\n");
+	string opcode = readStr();
+	if(strEql(opcode,"1")){
+		add();
+	}
+	else if(strEql(opcode,"2")){
+		sub();
+	}
+	else if(strEql(opcode,"3")){
+		multiply();
+	}
+	else if(strEql(opcode,"4")){
+		div();
+	}
+	else{
+		print("\nenter valid operation rerun the program\n");
+	}
 }
